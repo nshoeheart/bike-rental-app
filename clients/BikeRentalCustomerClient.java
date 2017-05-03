9b4d79ecccc10f9fb83679c3c2e4b27b50dab014
brgquist <alex-bergquist@uiowa.edu>
2017-05-03 13:07:21 -0500

    Update BikeRentalCustomerClient.java

--- clients/BikeRentalCustomerClient.java
+++ clients/BikeRentalCustomerClient.java
@@ -7,6 +7,8 @@ import util.ConnectionManager;
 
 import java.sql.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,17 +19,18 @@ import java.util.Scanner;
 public class BikeRentalCustomerClient {
 
     private Customer customer;
-
-    private static final int GET_AVAIL_BIKES = 1;
-
+    
+    private static final int EXIT = 0;
+    private static final int GET_AVAIL_BIKES = 1; // Scenario covers query b and transaction c
+    private static final int VIEW_CUST_RENTALS = 2; // Scenario covers query a and d
+    private static final int CANCEL_RESERVATION = 3; // Scenario covers transaction d
+    private static final int MAKE_RETURN = 4; // Scenario covers query c
+    
+    private Date currentdate = (Date) Date.from((LocalDate.now()).atStartOfDay(ZoneId.systemDefault()).toInstant());
+    
     public BikeRentalCustomerClient(Customer customer) {
         this.customer = customer;
-
-        System.out.println("Logged in to bike rental store customer client");
-        System.out.println("\tCustomer ID: " + customer.id);
-        System.out.println("\t  Last Name: " + customer.lastName);
-        System.out.println("\t First Name: " + customer.firstName);
-        System.out.println();
+        //todo "login" either by getting the customer's existing info from database or prompting for user creation
     }
 
     public void run() {
@@ -40,47 +43,87 @@ public class BikeRentalCustomerClient {
             // User input command, checked each loop
             int command = 1;
 
-            while (command != 0) {
+            while (command != EXIT) {
 
                 //Menu for commands, customer must enter 0 to exit app
                 System.out.print("Please enter the number of the command you wish to do:\n  0. Logout\n  1. Rent Bike\n  "
-                        + "2. Browse Equipment\n  3. Request a Service\n"); // 1. will enter into rentals table, 2 into equipment browser, 3 allows for services to be requested
-
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Rental r WHERE r.customer_id = ? AND r.checkout_date >= ?");
-                preparedStatement.setInt(1, customer.id);
-                preparedStatement.setDate(2, Date.valueOf(LocalDate.now()));
-                List<Rental> customerRentals = Rental.createListFromResultSet(preparedStatement.executeQuery());
+                        + "2. View Your Rentals\n  3. Request a Service\n");
 
                 //Check user input
                 command = Integer.parseInt(scanner.nextLine());
 
+                
+                /*
+                 * Exit case
+                 */
+                
                 if (command == 0) {
                     // Logs out and ends application
                     System.out.println("Logging out...");
+                    
+                /* 
+                 * View bikes available to Rent  
+                 * Covers query b and transaction c 
+                 * READY FOR TESTING  
+                 */
+                    
                 } else if (command == GET_AVAIL_BIKES) {
 
                     //Links to Rental table and returns all bikes that can be rented out
-                    int rent = 0;
+                    int rent = 1;
 
                     // Allows user to query to DB based on commands they select
-                    Statement statement1 = (Statement) connection.createStatement();
-                    ResultSet result1 = statement1.executeQuery("select * from Rental where checked_out = false"); // Returns in result all bikes on rental table available for rental
+                    Statement statement1 = connection.createStatement();
+                    ResultSet result1 = statement1.executeQuery("SELECT * FROM Rental r WHERE r.checked_out = false AND return_date != NULL"); // Returns in result all bikes on rental table available for rental
                     System.out.print("Please select one of the following bikes to rent by entering the id associated with the bike; "
-                            + "\n type '999' to go back to main menu:\n");
+                            + "\n type '0' to go back to main menu:\n");
 
                     List<Bicycle> availableBikes = Bicycle.createListFromResultSet(result1);
 
                     // Prints each result from Rental table that are available for customer to rent
-                    for (Bicycle bike : availableBikes) {
-                        System.out.println(); // bike data
+                    for (int i = 0; i < availableBikes.size(); i++){
+                        System.out.println(availableBikes.get(i).toString()); // bike data
                     }
 
                     rent = Integer.parseInt(scanner.nextLine());
+                    
+                    // Gets more info regarding the rental
+                    System.out.println("Enter the date you wish to rent the bike (format like MMM DD YYYY, including the spaces where MMM = Jan, Feb, etc.)");
+                    String in = scanner.nextLine();
+                    LocalDate date = LocalDate.parse(in); 
+                    
+                    Date checkout = (Date) Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant());
+                    
+                    
+                    // Boolean to check if customer will rent bike today; note: will checkout bike to customer if rental is same day
+                    Boolean bool = false;
+                    if(date == LocalDate.now()){bool = true;}
 
-                    if (rent != 999) { //Case where bike is rented, then we need to update the rental table
+                    System.out.println("How many days would you like to rent it?");
+                    int length = Integer.parseInt(scanner.nextLine());
+                    
+                    // Process for establishing rental dates
+                    LocalDate temp = date.plusDays(length);
+                    Date due = (Date) Date.from(temp.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+                    
+
+                    if (rent != 0) { //Case where bike is rented, then we need to update the rental table
                         try {
                             // If bike is rented, attempt to update it in database to have rented status
-                            PreparedStatement rentBike = (PreparedStatement) connection.prepareStatement("update Rental set checked_out = Y where Rental.id = " + String.valueOf(rent)); //queries and updates table approrpiately
+                        	PreparedStatement getBike = connection.prepareStatement("SELECT * FROM Rental r WHERE r.id = ?");
+                        	getBike.setInt(1, rent);
+                        	Rental referenceRental = Rental.createFromResultSetRow(getBike.executeQuery());
+                            PreparedStatement rentBike = connection.prepareStatement("INSERT INTO Rental (bike_id, customer_id, checkout_date, due_date, return_date, checked_out)"
+                            		+ " VALUES (? ? ? ? ? ?)"); //queries and updates table appropriately
+                            
+                            // Sets parameters of insertion accordingly, then executes
+                            rentBike.setInt(1, referenceRental.bikeId);
+                            rentBike.setInt(2, customer.id);
+                            rentBike.setDate(3, checkout);
+                            rentBike.setDate(4, due);
+                            rentBike.setDate(5, null);
+                            rentBike.setBoolean(6, bool);
                             rentBike.executeUpdate();
 
                         } catch (Exception e) {
@@ -89,78 +132,91 @@ public class BikeRentalCustomerClient {
                         }
                     }
 
+                    
+                /*
+                 *  View user's rentals
+                 *  Covers query a and d
+                 *  READY FOR TESTING
+                 */
+                    
+                } else if (command == VIEW_CUST_RENTALS) {
 
-                } else if (command == 2) {
+                    // Retrieves customer rentals
+                    PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Rental r WHERE r.customer_id = ? AND r.checkout_date >= ?");
+                    preparedStatement.setInt(1, customer.id);
+                    preparedStatement.setDate(2, new Date(LocalDate.now().toEpochDay()));
+                    List<Rental> customerRentals = Rental.createListFromResultSet(preparedStatement.executeQuery());
 
-                    // Allows user to shop equipment
-                    int select = 0;
-
-                    // Allows user to query to DB based on commands they select
-                    Statement statement2 = (Statement) connection.createStatement();
-                    ResultSet result2 = statement2.executeQuery("select * from Equpiment");
-                    System.out.print("Please browse the equipment available for purchase. If you wish to buy one, please type the id associated with it or 999 to exit:\n");
-
-                    // Prints out all available equipment from resulting query
-                    while (result2.next()) {
-                        System.out.print(result2.toString() + "\n");
+                    // Prints out customer's rentals
+                    for(int i = 0; i < customerRentals.size(); i++){
+                    	System.out.println(customerRentals.get(i).toString());
                     }
-
-                    // Read user input
-                    select = Integer.parseInt(scanner.nextLine());
-
-                    if (select == 999) {
+                    
+                /*
+                 *  Cancel a reservation before the check out date 
+                 *  Covers transaction d 
+                 */
+                    
+                }else if (command == CANCEL_RESERVATION){
+                	
+                	// Retrieves customer rentals beyond current date
+                    PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Rental r WHERE r.customer_id = ? AND r.checkout_date >= ?");
+                    preparedStatement.setInt(1, customer.id);
+                    preparedStatement.setDate(2, new Date(LocalDate.now().toEpochDay()));
+                    List<Rental> customerRentals = Rental.createListFromResultSet(preparedStatement.executeQuery());
+                    
+                    
+                    System.out.println("Which of these rentals would you like to cancel? Please enter the id number associated with the rental");
+                    
+                    // Prints out those customer's rentals
+                    for(int i = 0; i < customerRentals.size(); i++){
+                    	System.out.println(customerRentals.get(i).toString());
                     }
-                    ; // no updates/insert necessary for this if we assume we will always keep items in stock
-
-
-                } else if (command == 3) {
-
-                    // Request a service on a customer bike
-                    int service = 0;
-
-                    // Allows user to query DB based on input commands
-                    Statement statement3 = (Statement) connection.createStatement();
-                    ResultSet result3 = statement3.executeQuery("select * from OfferedService"); //Queues up all the available services shop can perform on bike
-                    System.out.print("Please select one of the following services to perform on your bike:\n");
-
-                    //Prints all available services from OfferedService table so customer can choose what they need
-                    while (result3.next()) {
-                        System.out.print(result3.toString() + "\n");
-                    }
-
-                    // Read user input
-                    service = Integer.parseInt(scanner.nextLine());
-
-                    // If service selected, asks customer about bike to prepare it for insertion into DB
-                    if (service != 999) {
-
-                        System.out.print("Please answer the following: \n What make of bike?");
-                        String make = scanner.nextLine();
-                        System.out.print("\n What model of bike?");
-                        String model = scanner.nextLine();
-                        System.out.println("\n Describe the bike's condition by one of the following words: Poor, Fair, Good, Great, New");
-                        String condition = scanner.nextLine();
-
-                        int b_id = 0; // I use this to get the correct bike id when we add a new one
-
-                        try {
-                            // Set up a new query
-                            Statement statement4 = (Statement) connection.createStatement();
-                            ResultSet result4 = statement4.executeQuery("select count(*) from Bicycle"); //gives total count of bikes
-                            // This returns the next bike ID
-                            b_id = result4.getInt(1) - 1;
-
-                            // Adds customer bike to Bicycle table for reference and performs the requested service
-                            PreparedStatement addBike = (PreparedStatement) connection.prepareStatement("insert into Bicycle values (" + b_id + ", " + make + ", " + model + ", " + condition + ", NULL");
-                            addBike.execute();
-                            PreparedStatement performService = (PreparedStatement) connection.prepareStatement("insert into PerformedService values (" + b_id + ", NULL");
-                            performService.execute();
-
-                        } catch (Exception e) {
-                            System.out.print("Failed to add bike and perform your requested service. Please try again.");
-                        }
-
-                    }
+                    
+                    // Gets user input
+                    int cancel = scanner.nextInt(); 
+                    
+                    
+                    // Deletes the rental from the table completely
+                    PreparedStatement cancelRental = connection.prepareStatement("DELETE FROM Rental where id = ?");
+                    cancelRental.setInt(1, cancel);
+                    cancelRental.executeQuery();
+                    
+                /*
+                * Make a return on a user rental
+                * Covers transaction b
+                * READY FOR TESTING
+                */    
+                }else if (command == MAKE_RETURN){
+                	
+                	// Queries up outstanding rentals for user and puts them in a list
+                	PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Rental r where r.customer_id = ? AND r.checked_out = true");
+                	preparedStatement.setInt(1, customer.id);
+                	List<Rental> outstandingRentals = Rental.createListFromResultSet(preparedStatement.executeQuery());
+                	
+                	System.out.println("Please select from the following outstanding rentals to return by typing the id number associated with the rental you wish to return.");
+                	// Prints outstanding rentals
+                	for (int i = 0; i < outstandingRentals.size(); i++){
+                		System.out.println(outstandingRentals.get(i).toString());
+                	}
+                	
+                	int ret = scanner.nextInt();
+                	
+                	PreparedStatement Return = connection.prepareStatement("UPDATE RENTAL r SET return_date = ? AND checked_out = false WHERE r.id = ?");
+                	Return.setDate(1, currentdate);
+                	Return.setInt(2, ret);
+                	Rental returnedRental = Rental.createFromResultSetRow(Return.executeQuery());
+                	Return.executeQuery();
+                	
+                	int daysdiff = currentdate.getDate() - returnedRental.dueDate.getDayOfMonth();
+                	float bikeCost = returnedRental.getBicycle(connection).costPerDay;
+                	float refund = daysdiff * bikeCost;
+                	
+                	System.out.println("Total refund is " + Float.toString(refund));
+                	
+                }else{
+                	System.out.println("Not a valid menu command. Please re-enter command option: ");
+                	command = Integer.parseInt(scanner.nextLine());
                 }
             }
 
