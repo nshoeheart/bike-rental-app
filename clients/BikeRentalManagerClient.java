e0954655a4e0d06e89c19a311993145ecbda306a
brgquist <alex-bergquist@uiowa.edu>
2017-05-03 23:34:23 -0500

    Update Manager Client
    Updated manager client to mimic the customer one. Note: the manager client assumes the manager knows what it is he intends to do instead of doing as much fishing around for data like in the customer client. For example, we assume the manager knows ids of certain things, like offered services, bikes, etc. (or at least has means to access them easily).

--- clients/BikeRentalManagerClient.java
+++ clients/BikeRentalManagerClient.java
@@ -1,6 +1,7 @@
 package clients;
 
 import models.Bicycle;
+import models.BikeCondition;
 import models.Customer;
 import models.Rental;
 import util.ConnectionManager;
@@ -55,36 +56,158 @@ public class BikeRentalManagerClient {
                 /*
                  * Managers view of all rentals 
                  * Covers query a
-                 * READY FOR TESTING
+                 * 
                  */
                 	
                 } else if (command == VIEW_CURRENT_RENTS) {
                 	
-                    PreparedStatement statement = connection.prepareStatement("SELECT * FROM Rental");
-                    List<Rental> Rentals =  Rental.createListFromResultSet(statement.executeQuery());
-                    
-                    for(int i = 0; i < Rentals.size(); i++){
-                    	System.out.println(Rentals.get(i).toString());
-                    }
+                    menuOption1();
 
                 } else if (command == PROCESS_CHECKOUT) {
+                	
+                	System.out.println("What is the customer's id number that is checking out?");
+                	int customer_id = scanner.nextInt();
+                	System.out.println("What is the bike's id number that the customer wishes to checkout?");
+                	int bike_id = scanner.nextInt();
                     
+                	menuOption2(customer_id, bike_id);
 
                 } else if (command == PROCESS_RETURN) {
-
+                	
+                	System.out.println("What is the rental id number that is being returned?");
+                	int rental_id = scanner.nextInt();
+                	
+                	menuOption3(rental_id);
                 	
                 } else if (command == VIEW_CONDITIONS){
-
+                	
+                	menuOption4();
                 	
                 } else if (command == EXECUTE_SERVICE){
-                
                 	
+                	System.out.println("What is the bike id of the bike being serviced?");
+                	int bike_id = scanner.nextInt();
+                	System.out.println("What is the service id of the service beind performed?");
+                	int service_id = scanner.nextInt();
+                	
+                	menuOption5(bike_id, service_id);
                 }
             }
             //todo insert manager client code here
         } catch (SQLException e) {
             ConnectionManager.rollbackConnection(connection);
             e.printStackTrace();
+        } finally {
+            ConnectionManager.closeConnection(connection);
+        }
+    }
+    
+    // Allows manager to see all rentals
+    private void menuOption1(){
+    	Connection connection = null;
+    	
+    	try{
+    		
+    		PreparedStatement statement = connection.prepareStatement("SELECT * FROM Rental");
+            List<Rental> Rentals =  Rental.createListFromResultSet(statement.executeQuery());
+            
+            for(int i = 0; i < Rentals.size(); i++){
+            	System.out.println(Rentals.get(i).toString());
+            }
+    		// If successful, commit transaction (otherwise should not reach this point)
+            connection.commit();
+        } catch (SQLException e) {
+            ConnectionManager.rollbackConnection(connection);
+        } finally {
+            ConnectionManager.closeConnection(connection);
+        }
+    }
+    
+    
+    // Allows a manager, assuming he knows the customer and bike id's of the rental
+    private void menuOption2(int customer_id, int bike_id){
+    	Connection connection = null;
+    	
+    	try{
+    		
+    		connection = ConnectionManager.getConnection();
+    		PreparedStatement update = connection.prepareStatement("UPDATE Rental r SET checked_out = true WHERE r.bikeId = ? AND r.customerId = ?");
+    		update.setInt(1, bike_id);
+    		update.setInt(2, customer_id);
+    		update.executeUpdate();
+    		
+    		// If successful, commit transaction (otherwise should not reach this point)
+            connection.commit();
+        } catch (SQLException e) {
+            ConnectionManager.rollbackConnection(connection);
+        } finally {
+            ConnectionManager.closeConnection(connection);
+        }
+    }
+    
+    
+    // Allows manager to process a return, assuming he knows the rental id that is to be returned
+    private void menuOption3(int rental_id){
+    	Connection connection = null;
+    	
+    	try{
+    		
+    		connection = ConnectionManager.getConnection();
+    		PreparedStatement update = connection.prepareStatement("UPDATE Rental r SET checked_out = false AND return_date = ? WHERE r.id = ?");
+    		update.setDate(1, Date.valueOf(LocalDate.now()));
+    		update.setInt(2, rental_id);
+    		update.executeUpdate();
+    		
+    		// If successful, commit transaction (otherwise should not reach this point)
+            connection.commit();
+        } catch (SQLException e) {
+            ConnectionManager.rollbackConnection(connection);
+        } finally {
+            ConnectionManager.closeConnection(connection);
+        }
+    }
+    
+    // Returns all bikes and their conditions: New, Good, etc.
+    private void menuOption4(){
+    	Connection connection = null;
+    	
+    	try{
+    		
+    		connection = ConnectionManager.getConnection();
+    		PreparedStatement conditions = connection.prepareStatement("SELECT Bicycle.id, Bicycle.model, Bicycle.make, BikeCondition.name FROM Bicycle JOIN BikeCondition ON Bicycle.bikeConditionId = Condition.id");
+    		List<BikeCondition> bikeConditions = BikeCondition.createListFromResultSet(conditions.executeQuery());
+    		System.out.println("Bicycles and their Conditions:\n");
+    		
+    		for(int i = 0; i < bikeConditions.size(); i++){
+    			System.out.println(bikeConditions.get(i));
+    		}
+    		
+    		// If successful, commit transaction (otherwise should not reach this point)
+            connection.commit();
+        } catch (SQLException e) {
+            ConnectionManager.rollbackConnection(connection);
+        } finally {
+            ConnectionManager.closeConnection(connection);
+        }
+    }
+    
+    // Performs selected service on selected bike on today's date
+    private void menuOption5(int bike_id, int service_id){
+    	Connection connection = null;
+    	
+    	try{
+    		
+    		connection = ConnectionManager.getConnection();
+    		PreparedStatement performService = connection.prepareStatement("INSERT INTO PerformedService (offeredServiceId, bikeId, datePerformed) VALUES (? ? ?)");
+    		performService.setInt(1, service_id);
+    		performService.setInt(2, bike_id);
+    		performService.setDate(3, Date.valueOf(LocalDate.now()));
+    		performService.executeUpdate();
+    		
+    		// If successful, commit transaction (otherwise should not reach this point)
+            connection.commit();
+        } catch (SQLException e) {
+            ConnectionManager.rollbackConnection(connection);
         } finally {
             ConnectionManager.closeConnection(connection);
         }
